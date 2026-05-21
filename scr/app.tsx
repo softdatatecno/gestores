22fddcb026b87d60d9875765bd66c07106c751a3
softdatatecno <softdatatecno@gmail.com>
2026-05-21 12:16:30 -0500

    Delete scr directory

--- scr/app.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{padding: '40px', textAlign: 'center'}}>
-      <h1>✅ ¡Tu App Funciona!</h1>
-      <p>App de Gestores - En línea</p>
-    </div>
-  );
-}
